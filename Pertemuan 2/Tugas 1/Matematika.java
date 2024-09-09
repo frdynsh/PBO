fc59f837352e2bb956c699e188e82d2e8fb5fc7d
FerdiYnsh <165828731+FerdiYnsh@users.noreply.github.com>
2024-09-09 13:10:55 +0700

    Update Matematika.java

--- Pertemuan 2/Tugas 1/Matematika.java
+++ Pertemuan 2/Tugas 1/Matematika.java
@@ -1,24 +1,24 @@
 package matematika;
-public class Matematika {
+public class Matematika implements InterfaceMatematika {
     int hasil;
     
      // Methode accessor
     public int getHasil() {
         return hasil;
     }
-    int pertambahan(int a,int b){
+    public int pertambahan(int a,int b){
         hasil = a+b;
         return hasil;
     }
-    int pengurangan(int a,int b){
+    public int pengurangan(int a,int b){
         hasil = a-b;
         return hasil;
     }
-    int perkalian(int a,int b){
+    public int perkalian(int a,int b){
         hasil = a*b;
         return hasil;
     }
-    int pembagian(int a,int b){
+    public int pembagian(int a,int b){
         hasil = a/b;
         return hasil;
     }
